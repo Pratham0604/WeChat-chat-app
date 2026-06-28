d3765beb471851238cb22756fe3556a4e57906a7
Pratham0604 <hmlsrivastava@gmail.com>
2026-06-28 12:24:42 +0530

    Set up Express server and MongoDB connection

---
+++ Backend/server.js
@@ -0,0 +1,62 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import http from "http";
+import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
+import { Server} from "socket.io"
+
+
+const app = express();
+const server = http.createServer(app)
+
+// console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
+// console.log("API Key:", process.env.CLOUDINARY_API_KEY);
+// console.log("API Secret:", process.env.CLOUDINARY_API_SECRET);
+
+// Initialize socket.io server
+export const io = new Server(server, {
+    cors: {origin: "*"}
+})
+
+// Store online users
+export const userSocketMap = {}; // { userId: socketId }
+
+// Socket.io connection handler
+io.on("connection", (socket)=>{
+    const userId = socket.handshake.query.userId;
+    console.log("User Connected", userId);
+
+    if(userId) userSocketMap[userId] = socket.id;
+
+    // Emit online users to all connected clients
+io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+socket.on("disconnect", ()=>{
+    console.log("User Disconnected", userId);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap))
+})
+
+})
+
+//middleware  
+
+app.use(express.json({limit : "4mb"}));
+app.use(cors());
+
+app.use("/api/status", (req,res)=> res.send("Server is live"));
+
+// routes
+
+app.use("/api/auth",userRouter);
+app.use("/api/messages",messageRouter);
+
+// connect to mongodb
+
+await connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, ()=> console.log("Server is running on PORT : "+ PORT));
